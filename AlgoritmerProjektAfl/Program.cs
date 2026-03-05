6a3c6f5bc54dadfc2999365637d777791b32f4f9
Dina Sakæussen <disa5@student.eadania.dk>
2026-03-05 15:39:48 +0100

    Add project files.

---
+++ AlgoritmerProjektAfl/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoritmerProjektAfl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
